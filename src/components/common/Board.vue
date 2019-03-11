04ab2dc5a66a18bea373537fbe41345bd5bf5fdb
Dmitry Nesterenko <nesterenko0602@gmail.com>
2019-03-11 15:09:31 +0200

    Board Component added

--- src/components/common/Board.vue
+++ src/components/common/Board.vue
@@ -1,21 +1,50 @@
 <template>
-  <Collection />
+  <div class="board__wrapper">
+    <TextHeader message="Boggle game" />
+    <Input
+      placeholder="Enter the word"
+      @submit="sendWord"
+    />
+    <Collection
+      :symbols="symbols"
+    />
+  </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 import Collection from 'components/ui/Collection/Collection';
+import Input from 'components/ui/Input';
+import TextHeader from 'components/ui/TextHeader';
 
 export default {
   name: 'Board',
   components: {
     Collection,
+    Input,
+    TextHeader,
   },
-  props: {
-  },
+  data: () => ({
+    symbols: ['A', 'C', 'E', 'D', 'L', 'U', 'G', '*', 'E', '*', 'H', 'T', 'G', 'A', 'F', 'K'],
+  }),
   methods: {
+    ...mapActions([
+      'sendWord',
+    ]),
   },
 };
 </script>
 
 <style lang="scss" scoped>
+  .board__wrapper {
+    background: white;
+    border-radius: 8px;
+    box-shadow: 0 0 32px 8px rgba(0,0,0,.1);
+    box-sizing: border-box;
+    margin: 20px;
+    min-height: 300px;
+    padding: 12px 56px 32px;
+    width: 500px;
+  }
 </style>
