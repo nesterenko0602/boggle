05237aa5c4d4768b54e7e85a71e468a7d515fda3
Dmitry Nesterenko <nesterenko0602@gmail.com>
2019-03-11 15:44:54 +0200

    ComponentItem added to print passed letter

--- src/components/ui/Collection/CollectionItem.vue
+++ src/components/ui/Collection/CollectionItem.vue
@@ -1,16 +1,35 @@
 <template>
-  <div class="collection-item__wrapper" />
+  <div class="collection-item__wrapper">
+    {{ item }}
+  </div>
 </template>
 
 <script>
 export default {
   name: 'CollectionItem',
   props: {
-  },
-  methods: {
+    item: {
+      type: String,
+      required: true,
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+@import 'styles/variables.scss';
+
+.collection-item__wrapper {
+  align-items: center;
+  background: $grey-lighter;
+  border-radius: 3px;
+  cursor: default;
+  display: flex;
+  font-size: 36px;
+  height: 50px;
+  justify-content: center;
+  margin: 2px;
+  pointer-events: none;
+  width: 50px;
+}
 </style>
