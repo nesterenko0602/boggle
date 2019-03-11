1909e9df56d3115ca3b993be10afbeb36e5fa8fe
Dmitry Nesterenko <nesterenko0602@gmail.com>
2019-03-12 00:50:19 +0200

    Mock API helper

---
+++ src/helpers/Api.js
@@ -0,0 +1,50 @@
+/**
+ * Mock API
+ */
+
+const mockData = {
+  configuration: ['A', 'C', 'E', 'D', 'L', 'U', 'G', '*', 'E', '*', 'H', 'T', 'G', 'A', 'F', 'K'],
+};
+
+/**
+ * Returns a random response delay for requests
+ */
+function getRandomDelay() {
+  const max = 1000;
+  const min = 200;
+
+  return Math.random() * (max - min) + min;
+}
+
+/**
+ * 
+ * @param {Function} fn Function to invoke (resolver or rejector)
+ * @param {any} body Reponse body
+ */
+function response(fn, body) {
+  setTimeout(() => {
+    fn(body);
+  }, getRandomDelay());
+}
+
+export default {
+  get(url) {
+    let resolve = null;
+    let reject = null;
+
+    const responsePromise = new Promise((innerResolve, innerReject) => {
+      resolve = innerResolve;
+      reject = innerReject;
+    });
+
+    switch (url) {
+      case '/getConfiguration':
+        response(resolve, mockData.configuration)
+        break;
+      default:
+        break;
+    }
+
+    return responsePromise;
+  },
+};
