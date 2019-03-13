aab7db9e94491d9a8da15368784a576d92e72fb3
Dmitry Nesterenko <nesterenko0602@gmail.com>
2019-03-13 17:46:51 +0200

    ErrorMessage component test

---
+++ tests/unit/ui/ErrorMessage.spec.js
@@ -0,0 +1,36 @@
+import { shallowWithRequiredProps } from 'tests/helpers';
+import ErrorMessage from 'components/ui/ErrorMessage';
+
+const errorText = 'ErrorText';
+const getterWithError = {
+  getters: {
+    getError: () => errorText,
+  },
+};
+
+const getterWithoutError = {
+  getters: {
+    getError: () => null,
+  },
+};
+
+const getWrapper = shallowWithRequiredProps(ErrorMessage);
+
+describe('ErrorMessage', () => {
+  it('renders error text', () => {
+    const wrapper = getWrapper({}, getterWithError);
+
+    expect(wrapper.text()).toBe(errorText);
+  });
+  
+  it('applies visibility class when error', () => {
+    [false, true].forEach((hasError) => {
+      const wrapper = getWrapper({}, hasError ? getterWithError : getterWithoutError);
+
+      expect(
+        wrapper.find('.error-message__wrapper').classes('error-message__wrapper--visible')
+      )
+        .toBe(hasError);
+    });
+  });
+});
