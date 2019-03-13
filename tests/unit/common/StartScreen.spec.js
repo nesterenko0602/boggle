67a4ef8495972426793f01e07b093c708f2c2899
Dmitry Nesterenko <nesterenko0602@gmail.com>
2019-03-13 21:09:04 +0200

    StartGame component test

---
+++ tests/unit/common/StartScreen.spec.js
@@ -0,0 +1,25 @@
+import { shallowWithRequiredProps } from 'tests/helpers';
+
+import StartScreen from 'components/common/GameScreens/StartScreen';
+import Button from 'components/ui/Button';
+import { GAME_DURATION } from 'constants/constants';
+
+const getWrapper = shallowWithRequiredProps(StartScreen);
+
+describe('StartScreen', () => {
+  it('renders correct game duration value', () => {
+    const wrapper = getWrapper();
+    
+    expect(wrapper.find('.start-screen__rules b').text()).toBe(`${GAME_DURATION} seconds`);
+  });
+  
+  it('emits on button click', () => {
+    const startGame = jest.fn();
+    const wrapper = getWrapper({}, { actions: { startGame } });
+
+    wrapper.find(Button).vm.$emit('click');
+
+    expect(startGame)
+      .toHaveBeenCalled();
+  });
+});
