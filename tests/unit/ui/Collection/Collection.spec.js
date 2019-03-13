2ad1338acfe6dd86b9152e79edfa449528f5063b
Dmitry Nesterenko <nesterenko0602@gmail.com>
2019-03-13 15:43:04 +0200

    Collection component test

---
+++ tests/unit/ui/Collection/Collection.spec.js
@@ -0,0 +1,20 @@
+import { shallowWithRequiredProps } from 'tests/helpers';
+import Collection from 'components/ui/Collection/Collection';
+import CollectionItem from 'components/ui/Collection/CollectionItem';
+
+const items = ['T', 'A', 'P', '*', 'E', 'A', 'K', 'S', 'O', 'B', 'R', 'S', 'S', '*', 'X', 'D'];
+
+const getWrapper = shallowWithRequiredProps(
+  Collection,
+  {
+    props: { items },
+  },
+);
+
+describe('Collection', () => {
+  it('renders passed items', () => {
+    const wrapper = getWrapper({ items });
+
+    expect(wrapper.findAll(CollectionItem).length).toBe(items.length);
+  });
+});
