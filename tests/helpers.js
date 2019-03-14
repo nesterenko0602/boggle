ad42e1f5452eb74c9339e53d5a242c9955992f4d
Dmitry Nesterenko <nesterenko0602@gmail.com>
2019-03-14 17:43:08 +0200

    Added states helper for testing mutations

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,6 +1,9 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 import Vuex from 'vuex';
 
+import { CHECK_STATUSES, GAME_DURATION } from 'constants/constants';
+import initState from 'store/state';
+
 /**
  * Create mounting factory function with fixed component
  * and set of required props, options, getters
@@ -47,3 +50,43 @@ export const shallowWithRequiredProps = (
     },
   );
 };
+
+/**
+ * Imitation of regular in game state
+ */
+const normalGameState = {
+  error: null,
+  configuration: ['T', 'A', 'P', '*', 'E', 'A', 'K', 'S', 'O', 'B', 'R', 'S', 'S', '*', 'X', 'D'],
+  attempts: [
+    { word: 'word1', time: 1552509631343, status: CHECK_STATUSES.PENDING },
+    { word: 'word2', time: 1552509631344, status: CHECK_STATUSES.PENDING },
+    { word: 'word3', time: 1552509631345, status: CHECK_STATUSES.PENDING },
+    { word: 'word4', time: 1552509631346, status: CHECK_STATUSES.SUCCESS },
+    { word: 'word5', time: 1552509631347, status: CHECK_STATUSES.SUCCESS },
+    { word: 'word6', time: 1552509631348, status: CHECK_STATUSES.ERORR },
+  ],
+  startTime: new Date().getTime() - 1000 * parseInt(GAME_DURATION / 2, 10),
+  isLoading: false,
+  isFinished: false,
+};
+
+/**
+ * Get set of app states for various situation
+ *
+ * @returns {Object}
+ */
+export const getVariousStates = () => JSON.parse(
+  JSON.stringify({
+    start: initState,
+    game: normalGameState,
+    finish: {
+      ...normalGameState,
+      startTime: new Date().getTime() - 1000 * GAME_DURATION,
+      isFinished: true,
+    },
+    error: {
+      ...normalGameState,
+      error: 'Some Error',
+    }
+  })
+);
